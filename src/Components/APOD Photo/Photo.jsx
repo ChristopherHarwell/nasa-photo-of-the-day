628697c110b3d0dc6367862562e98e1b5d80ea73
Chris <chrisharwell72@gmail.com>
2020-04-29 22:15:35 -0400

    added MaterialUI styling

--- src/Components/APOD Photo/Photo.jsx
+++ src/Components/APOD Photo/Photo.jsx
@@ -1,21 +1,53 @@
-import React from 'react';
-import {Card, CardContent, CardMedia, CardHeader, TextField} from '@material-ui/core';
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  CardActionArea,
+  Typography,
+} from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import { findByLabelText } from "@testing-library/react";
 
+const useStyles = makeStyles({
+    card: {
+        border: '2px solid black',
+        margin: '5vh 60vh',
+        padding: '5vh 0',
+    },
+    media: {
+        display: 'flex',
+        flexDirection: 'column',
+        marginLeft: '30vh',
+        maxHeight: '50vh',
+        maxWidth: '100vh',
+    },
+    typogrophy: {
+        display: 'flex',
+        flexDirection: 'column',
+        marginLeft: '40vh',
+        maxWidth: '80vh',
+        maxHeight: '60vh'
+    },
+});
 const APODPhoto = (props) => {
-    if(!props.url) {
-        return (
-            <h3>Loading...</h3>
-        );
-    }
-    return (
-        <div>
-            <Card>
-                <CardHeader title={props.title}></CardHeader>
-                <CardMedia component='img' src={props.url} alt={props.title}/>
-                <CardContent children={props.explanation}></CardContent>
-            </Card>
-        </div>
-    );
+  const classes = useStyles();
+  if (!props.url) {
+    return <h3>Loading...</h3>;
+  }
+  return (
+    <div>
+      <Card className={classes.card}>
+        <CardActionArea>
+          <CardMedia className={classes.media} component="img" src={props.url} alt={props.title} />
+          <CardContent className={classes.typogrophy}>
+            <Typography>{props.title}</Typography>
+            <Typography>{props.explanation}</Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </div>
+  );
 };
 
 export default APODPhoto;
